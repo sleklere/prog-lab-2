e53995cd30a4b5ab21e9c2bd05ebcce5a698f4cc
sleklere <santiagoleklere@gmail.com>
2024-04-17 21:12:18 -0300

    prog - articul y here+compo avanzado

--- programacion/clase-3/ej-herencia-composicion.cpp
+++ programacion/clase-3/ej-herencia-composicion.cpp
@@ -11,14 +11,15 @@ Empleado::Empleado(int categoria, float sueldo, const Fecha fechaIngreso) {
   _sueldo = sueldo;
   _fechaIngreso = fechaIngreso;
 }
+
 const char* Proveedor::getCUIL() { return _CUIL; }
 int Proveedor::getTipoProducto() { return _tipoProducto; }
 char Proveedor::getCondicionPago() { return _condicionPago; }
+
 bool Proveedor::setCUIL(const char* CUIL) {
   if (CUIL == nullptr) {
     return false;
   }
-  cout << sizeof CUIL;
   if (strlen(CUIL) == 11) {
     strcpy(_CUIL, CUIL);
     return true;
@@ -41,6 +42,37 @@ bool Proveedor::setCondicionPago(char condicionPago) {
     return false;
   }
 }
+
+void Proveedor::Cargar() {
+  Persona::Cargar();
+  char CUIL[11], condicionPago;
+  int tipoProducto;
+  bool inputValido = 0;
+
+  cout << "CUIL: ";
+  cin >> CUIL;
+  setCUIL(CUIL);
+  // 1 ejemplo de usar la validacion del setter en la carga
+  while (!inputValido) {
+    cout << "Condicion de pago: ";
+    cin >> condicionPago;
+    if (setCondicionPago(condicionPago)) {
+      inputValido = 1;
+    } else {
+      cout << "Input invalido...intente de nuevo." << endl;
+    }
+  }
+  cout << "Tipo de producto: ";
+  cin >> tipoProducto;
+  setTipoProducto(tipoProducto);
+}
+void Proveedor::Mostrar() {
+  Persona::Mostrar();
+  cout << "CUIL: " << getCUIL() << endl;
+  cout << "Condicion de pago: " << getCondicionPago() << endl;
+  cout << "Tipo de producto: " << getTipoProducto() << endl;
+}
+
 Proveedor::Proveedor(const char* CUIL = "203902805", int tipoProducto = 7,
                      char condicion = 'a') {
   // strcpy(_CUIL, CUIL);
@@ -48,6 +80,7 @@ Proveedor::Proveedor(const char* CUIL = "203902805", int tipoProducto = 7,
   _tipoProducto = tipoProducto;
   _condicionPago = condicion;
 }
+
 Docente::Docente(const char* legajo, const char* cargo, Fecha fechaIngreso) {
   strcpy(_legajo, legajo);
   strcpy(_cargo, cargo);
@@ -55,8 +88,15 @@ Docente::Docente(const char* legajo, const char* cargo, Fecha fechaIngreso) {
 }
 
 int main() {
+  Empleado emp1;
   Proveedor prov1;
+  Docente doce1;
+  // emp1.Cargar();
+  // cout << endl;
   prov1.Cargar();
+  cout << endl;
+  // doce1.Cargar();
+  // cout << endl;
   system("pause");
   prov1.Mostrar();
   return 0;
